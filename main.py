eb43a3bf79785cc3033a23fef9773d15a582a8a4
Grin-de-alt <125186183+Grin-de-alt@users.noreply.github.com>
2023-03-13 19:23:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def parse():
     employees = page_parsed.findAll('div', class_="css-17lk78h e3f4v4l2") 
     with open('result.txt', 'w') as f: 
         for employee in employees: 
-            name = employee.find('span') 
-            f.write(name + '\n') 
+            auto = employee.find('span') 
+            f.write(auto + '\n') 
 
