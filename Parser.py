72d23ff816b809ee53d290d17ac7c793477fddcc
Grin-de-alt <125186183+Grin-de-alt@users.noreply.github.com>
2023-04-11 16:26:56 +0600

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -8,10 +8,8 @@ def parse():
     autos = page_parsed.findAll('div', class_='css-l1wt7n e3f4v4l2')
     autos1 = page_parsed.findAll('span', class_='css-46itwz e162wx9x0')
     with open('autos.txt', 'w') as f:
-        for auto in autos:
+        for auto, auto1 in zip(autos, autos1):
             name = auto.find('span').text.strip()
-            for auto1 in autos1:
-                name1 = auto1.find('span').text.strip()
-                f.write('Модель: ' + name + ' года. Цена: ' + name1 + ' руб.' '\n')
-                break
+            name1 = auto1.find('span').text.strip()
+            f.write('Модель: ' + name + ' года. Цена: ' + name1 + ' руб.' '\n')
     f.close(autos.txt)
